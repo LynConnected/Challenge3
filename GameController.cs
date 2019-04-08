a32928bf76e839e81fc7fbdeec1f18606255f037
Ayame <46459682+LynConnected@users.noreply.github.com>
2019-04-07 20:32:45 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text pointsText;
+    public Text restartText;
+    public Text gameOverText;
+
+    private int points;
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        points = 0;
+        UpdatePoints();
+        StartCoroutine (SpawnWaves());
+    }
+    
+    void Update()
+    {
+        pointsText.text = "Points:" + points;
+        if (points >= 100)
+        {
+            gameOverText.text = "GAME CREATED BY CAITLIN CONRAD";
+            gameOver = true;
+            restart = true;
+        }
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.S))
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'S' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        points += newScoreValue;
+        UpdatePoints();
+    }
+
+    void UpdatePoints()
+    {
+        pointsText.text = "Points: " + points;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
